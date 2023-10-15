11b4175f3047e9a1dfc101b7c74a556876df4727
Vivek Chittepu <44039386+vivekranadive@users.noreply.github.com>
2023-10-14 21:35:50 -0400

    Revert "homescreen"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import HomeScreen from "./HomeScreen";
 import Tabs from '../app/Tabs';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <HomeScreen />
       <Tabs />
     </main>
   );
